585c1769857447ba404401ae8aba29ffab2f61e6
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-02-09 20:24:45 +0100

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,39 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor () {
+    this.axiosApp = axios.create({
+      baseURL : 'https://minions-api.herokuapp.com'
+    })
   }
 
   getFullList () {
+    return this.axiosApp.get('/characters')
+    
+  }
+
+  getOneRegister (id) {
+    
+    return this.axiosApp.get(`/characters/${id}`)
 
   }
 
-  getOneRegister () {
+  createOneRegister (values) {
+
+    console.log(values)
+
+    return this.axiosApp.post('/characters', values)
+    
+  }
+
+  updateOneRegister (id, data) {
+    
+    return this.axiosApp.put(`/characters/${id}`, data )
+
 
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister (id) {
+    
+    return this.axiosApp.delete(`/characters/${id}`)
 
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,143 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler();
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+
+
+window.addEventListener('load', () => { //mostrar todos
+  document.getElementById('fetch-all').addEventListener('click', () => {
+
+    document.querySelector('.characters-container').innerHTML = ``
+
+    let charactersArray = []
+
+    charactersAPI
+      .getFullList()
+      .then((characters) => {
+        characters.data.forEach(element => {
+          charactersArray.push(element)
+
+        })
+      })
+
+      .then(() => {
+        charactersArray.forEach(minion => (
+          document.querySelector('.characters-container').innerHTML += `
+      <div class="character-info">
+        <div class="name">Name${minion.name}</div>
+        <div class="occupation">Occupation:${minion.occupation}</div>
+        <div class="cartoon">Is a Cartoon?${minion.cartoon}</div>
+        <div class="weapon">Character Weapon: ${minion.weapon}</div>
+      </div>`
+        ))
+
+      })
+      .catch(console.log('La has liado sacando los personajes', charactersArray))
+
+
+  }
+
+  );
+
+  // saca uno
+
+  document.getElementById('fetch-one').addEventListener('click', () => {
+
+
+
+    let id = document.querySelectorAll('.operation input')[0].value
+
+    let name = document.querySelector('.name')
+    let occupation = document.querySelector('.occupation')
+    let cartoon = document.querySelector('.cartoon')
+    let weapon = document.querySelector('.weapon')
+
+    let insert = {}
+
+    charactersAPI
+      .getOneRegister(id)
+      .then((character) => {
+        insert.character = character
+
+        name.innerHTML = `Name: ${insert.character.data.name}`
+        occupation.innerHTML = `Occupation: ${insert.character.data.occupation}`
+        cartoon.innerHTML = `Cartoon: ${insert.character.data.cartoon}`
+        weapon.innerHTML = `Weapon: ${insert.character.data.weapon}`
+
+        document.querySelectorAll('#edit-character-form input')[0].value = insert.character.data.id
+        document.querySelectorAll('#edit-character-form input')[1].value = insert.character.data.name
+        document.querySelectorAll('#edit-character-form input')[2].value = insert.character.data.occupation
+        document.querySelectorAll('#edit-character-form input')[3].value = insert.character.data.weapon
+        document.querySelectorAll('#edit-character-form input')[4].checked = insert.character.data.cartoon
+
+
+
+      })
+      .catch(console.log('La has liado en fetch one', insert))
+
+
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  // borrar uno
+
+  document.getElementById('delete-one').addEventListener('click', () => {
+
+    const id = document.querySelector('.operation.delete input').value
+    // console.log(id)
+    charactersAPI
+      .deleteOneRegister(id)
+      .then(()=>document.querySelector('#delete-one').classList.toggle('correct'))
+
+      .catch(()=>document.querySelector('#delete-one').classList.toggle('wrong'))
+
+
 
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  //editar uno
+
+  document.getElementById('edit-character-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const id = document.querySelectorAll('#edit-character-form input')[0].value
+    const name = document.querySelectorAll('#edit-character-form input')[1].value
+    const occupation = document.querySelectorAll('#edit-character-form input')[2].value
+    const weapon = document.querySelectorAll('#edit-character-form input')[3].value
+    const cartoon = document.querySelectorAll('#edit-character-form input')[4].checked
+    const data = { name, occupation, weapon, cartoon }
+
+    charactersAPI
+      .updateOneRegister(id, data)
+      .then(()=>document.querySelector('#update-data').classList.toggle('correct'))
+      .catch(()=>document.querySelector('#update-data').classList.toggle('wrong'))
 
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
-  });
+  // crear uno
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.getElementById('new-character-form').addEventListener('submit', (e) => {
+    e.preventDefault() //para que el formulario recargue la pagina
+
+    const name = document.querySelectorAll('#new-character-form input')[0].value
+    const occupation = document.querySelectorAll('#new-character-form input')[1].value
+    const weapon = document.querySelectorAll('#new-character-form input')[2].value
+    const cartoon = document.querySelectorAll('#new-character-form input')[3].checked
+
+    console.log(name, occupation, weapon, cartoon)
+
+    const data = { name, occupation, weapon, cartoon }
+
+    console.log(data)
+
+    charactersAPI
+      .createOneRegister(data)
+      .then(()=> document.querySelector('#create-data').classList.toggle('correct'))
+      .catch(()=> document.querySelector('#create-data').classList.toggle('wrong'))
+
+
+
+
+
 
   });
 });
